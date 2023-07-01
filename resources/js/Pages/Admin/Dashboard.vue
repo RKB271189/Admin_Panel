0a3248e4b733f0de301d43bd8a6c9a9c5d3b2b57
Rahul B <rahul.b@avdevs.com>
2023-07-01 14:59:24 -0600

    adding few reusable components

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,10 +1,75 @@
 <template>
-  <h1>This is dashboard</h1>
+  <v-app>
+    <Header></Header>
+    <Menu :page-name="pageName"></Menu>
+    <v-main>
+      <v-container class="fluid">
+        <v-row>
+          <v-col md="3">
+            <count-box
+              :boxColor="'info'"
+              :textColor="'white'"
+              :countNumber="'25'"
+              :countTitle="'User'"
+              :iconMain="'mdi-account-star'"
+              :moreInfoBackColor="'#007bff'"
+              :navigateURL="'login'"
+              :enableAction="true"
+            ></count-box>
+          </v-col>
+          <v-col md="3">
+            <count-box
+              :boxColor="'#ffc107'"
+              :textColor="'black'"
+              :countNumber="'150'"
+              :countTitle="'Orders'"
+              :iconMain="'mdi-basket-plus'"
+              :moreInfoBackColor="'#fd7e14'"
+              :navigateURL="'login'"
+              :enableAction="true"
+            ></count-box>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6" md="6">
+            <v-card>
+              <v-card-title class="alert-warning">
+                <span class="headline">Some Title</span>
+                <v-btn
+                  :loading="false"
+                  class="flex-grow-1"
+                  height="48"
+                  variant="tonal"
+                  @click="load"
+                  style="float: right"
+                >
+                  <v-icon class="mr-1">mdi-login</v-icon>
+                  Some Action
+                </v-btn>
+              </v-card-title>
+              <v-card-text></v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {};
+import Header from "./Layout/Header.vue";
+import Menu from "./Layout/Menu.vue";
+import CountBox from "../General/Count-Box.vue";
+export default {
+  data() {
+    return {
+      pageName: "Dashboard",
+    };
+  },
+  components: {
+    Header,
+    Menu,
+    CountBox,
+  },
+};
 </script>
-
-<style>
-</style>
---
+++ resources/js/Pages/Admin/Layout/Menu.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-list>
+      <v-list-item-group v-model="selectedItem">
+        <v-list-item
+          v-for="(item, index) in menuItems"
+          :key="index"
+          @click="toggleSubMenu(item)"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-icon>{{ item.icon }}</v-icon>
+              <span class="menu-name">{{ item.title }}</span>
+              <v-icon class="arrow-icon" v-if="item.submenu">{{
+                getSubMenuIcon(item)
+              }}</v-icon>
+            </v-list-item-title>
+          </v-list-item-content>
+          <transition name="fade">
+            <v-list-item-group
+              v-if="item.submenu && item.open"
+              v-model="selectedSubItem"
+            >
+              <v-list-item
+                v-for="(subItem, subIndex) in item.submenu"
+                :key="subIndex"
+                @click.stop
+              >
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <v-icon>{{ subItem.icon }}</v-icon>
+                    <span class="menu-name">{{ subItem.title }}</span>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </transition>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>{{ pageName }}</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  props: {
+    pageName: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      drawer: false,
+      selectedItem: null,
+      selectedSubItem: null,
+      menuItems: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        {
+          title: "CMS",
+          icon: "mdi-page-layout-body",
+          submenu: [
+            { title: "Home", icon: "mdi-menu-right-outline" },
+            { title: "About", icon: "mdi-menu-right-outline" },
+            { title: "Contact", icon: "mdi-menu-right-outline" },
+          ],
+          open: false,
+        },
+        { title: "Password", icon: "mdi-cog" },
+        {
+          title: "Users",
+          icon: "mdi-account-group",
+          submenu: [
+            { title: "Student", icon: "mdi-menu-right-outline" },
+            { title: "Worker", icon: "mdi-menu-right-outline" },
+            { title: "Consultant", icon: "mdi-menu-right-outline" },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleSubMenu(item) {
+      if (item.submenu) {
+        if (item.open) {
+          item.open = false;
+        } else {
+          this.menuItems.forEach((menu) => {
+            if (menu !== item) {
+              menu.open = false;
+            }
+          });
+          item.open = true;
+        }
+      } else {
+        this.menuItems.forEach((menu) => {
+          if (menu !== item) {
+            menu.open = false;
+          }
+        });
+      }
+    },
+    getSubMenuIcon(item) {
+      return item.open ? "mdi-chevron-down" : "mdi-chevron-right";
+    },
+  },
+};
+</script>
